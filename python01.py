b99e7d10b0729bfcda72b3b42c06bd865ffcb26b
Danger <1319277086@qq.com>
2017-09-13 21:55:59 +0800

    test

---
+++ python01.py
@@ -0,0 +1,7 @@
+print("我就是神");print("你们的神");print("哈哈被忽悠了吧");
+a=3
+if(a>0):
+    print("我是好人")
+    print("我是好人")
+    print("我是好人")
+else:print("我是坏人")
---
+++ whilexunhuan.py
@@ -0,0 +1,14 @@
+#while 循环
+count=0
+while(count<9):
+    print("次数是:",count)
+    count+=1
+
+
+
+
+    num=1000 #整形
+    strhe="你好中国" #string类型
+    strhe1='我很好' #string类型  单引号双引号都一样表示字符串
+    ffff=1000.1 #浮点型
+    print(num);print(strhe);print(strhe1);print(ffff);
